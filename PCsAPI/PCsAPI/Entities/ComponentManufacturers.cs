66e73309682a1df2e4a89f4967447a178233d1a1
Kacper <kacper.piotrowski1711@gmail.com>
2026-05-16 17:56:18 +0200

    Add ComponentManufacturers Entity

---
+++ PCsAPI/PCsAPI/Entities/ComponentManufacturers.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PCsAPI.Entities;
+
+public class ComponentManufacturers
+{
+    [Key]
+    public int Id { get; set; }
+    
+    [MaxLength(30)]
+    public string Abbreviation { get; set; } = String.Empty;
+    
+    [MaxLength(300)]
+    public string FullName { get; set; } = String.Empty;
+    
+    public DateOnly FoundationDate { get; set; } 
+    
+    public ICollection<Components> Components { get; set; } = [];
+}
