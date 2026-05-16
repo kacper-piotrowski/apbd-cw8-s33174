c1ca7625f85cf016b1393a9af821f6092b8aa6d3
Kacper <kacper.piotrowski1711@gmail.com>
2026-05-16 17:34:34 +0200

    Add PCs Entity

---
+++ PCsAPI/PCsAPI/Entities/PCs.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PCsAPI.Entities;
+
+public class PCs
+{
+    [Key]
+    public int Id { get; set; }
+    
+    [MaxLength(50)]
+    public string Name { get; set; } = String.Empty;
+    
+    [Column(TypeName = "float(5)")]
+    public float Weight { get; set; }
+    
+    public int Warranty { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+    
+    public int Stock { get; set; }
+
+    public ICollection<PCComponents> PcComponents { get; set; } = [];
+}
