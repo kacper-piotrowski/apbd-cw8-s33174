807a8fa052fe649fd0514f45f3598f9ec1eaa824
Kacper <kacper.piotrowski1711@gmail.com>
2026-05-16 17:58:42 +0200

    Add ComponentTypes Entity

---
+++ PCsAPI/PCsAPI/Entities/ComponentTypes.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PCsAPI.Entities;
+
+public class ComponentTypes
+{
+    [Key]
+    public int Id { get; set; }
+    
+    [MaxLength(30)]
+    public string Abbreviation { get; set; } = String.Empty;
+    
+    [MaxLength(150)]
+    public string Name { get; set; } = String.Empty;
+
+    public ICollection<Components> Components { get; set; } = [];
+}
