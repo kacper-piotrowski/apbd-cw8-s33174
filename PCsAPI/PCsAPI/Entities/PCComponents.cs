c9c9108f1911fc55830372a0e54b8bf953b64661
Kacper <kacper.piotrowski1711@gmail.com>
2026-05-16 17:42:45 +0200

    Add PCComponents Entity

---
+++ PCsAPI/PCsAPI/Entities/PCComponents.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+ 
+namespace PCsAPI.Entities;
+
+[PrimaryKey(nameof(PCId), nameof(ComponentCode))]
+public class PCComponents
+{
+    public int PCId {get; set;}
+    
+    [Column(TypeName = "char(10)")]
+    [MaxLength(10)]
+    public string ComponentCode {get; set;}
+    
+    public int Amount {get; set;}
+
+    public PCs Pc { get; set; } = null;
+    public Components Component { get; set; } = null;
+}
